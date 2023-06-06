6e483d788dd3f75dcbe2f226bc8df69bf2b2842e
shchegolenkov <denis.amorales@ya.ru>
2023-06-06 20:15:39 +0300

    feat: add typescript settings

--- news-JS/src/components/controller/appLoader.js
+++ news-JS/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '56942e6cf42d46f39dd87ccd467347f9', // получите свой ключ https://newsapi.org/
         });
     }
 }
