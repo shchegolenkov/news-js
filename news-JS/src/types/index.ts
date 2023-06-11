933dafa1b62d5597720d3bba09c9b32f149a2599
shchegolenkov <denis.amorales@ya.ru>
2023-06-11 22:58:10 +0300

    feat: create source and article interfaces

---
+++ news-JS/src/types/index.ts
@@ -0,0 +1,34 @@
+export interface Source {
+    id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+}
+
+export interface NewsAPIResponse {
+    status: string;
+    sources: Source[];
+}
+
+export interface Article {
+    source: {
+        id: string | null;
+        name: string;
+    };
+    author: string | null;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string | null;
+    publishedAt: string;
+    content: string;
+}
+
+export interface NewsResponse {
+    status: string;
+    totalResults: number;
+    articles: Article[];
+}
