e39fe95f866d76e8a7de616d12f48abd0c0d0560
shchegolenkov <denis.amorales@ya.ru>
2023-06-12 01:19:55 +0300

    feat: migrate appView to ts

---
+++ news-JS/src/components/view/appView.ts
@@ -0,0 +1,25 @@
+import News from './news/news';
+import Sources from './sources/sources';
+import { NewsAPIResponse, NewsResponse } from '../../types/index';
+
+export class AppView {
+    private news: News;
+    private sources: Sources;
+
+    constructor() {
+        this.news = new News();
+        this.sources = new Sources();
+    }
+
+    public drawNews(data: NewsResponse): void {
+        const values: NewsResponse['articles'] = data?.articles ? data?.articles : [];
+        if (values.length) this.news.draw(values);
+    }
+
+    public drawSources(data: NewsAPIResponse): void {
+        const values: NewsAPIResponse['sources'] = data?.sources ? data?.sources : [];
+        if (values.length) this.sources.draw(values);
+    }
+}
+
+export default AppView;
