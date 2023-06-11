4cfb6fa65601b8bcc88537375f070aa52428bf38
shchegolenkov <denis.amorales@ya.ru>
2023-06-12 00:14:27 +0300

    feat: migrate sources to ts

---
+++ news-JS/src/components/view/sources/sources.ts
@@ -0,0 +1,27 @@
+import './sources.css';
+import { Source } from '../../../types/index';
+
+class Sources {
+    public draw(data: Source[]): void {
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const sourceItemTemp: HTMLTemplateElement | null = document.querySelector('#sourceItemTemp');
+
+        if (sourceItemTemp instanceof HTMLTemplateElement) {
+            data.forEach((item) => {
+                const sourceClone: Node = sourceItemTemp.content.cloneNode(true);
+                if (sourceClone instanceof DocumentFragment) {
+                    const sourceItemName: Element | null = sourceClone.querySelector('.source__item-name');
+                    if (sourceItemName !== null) sourceItemName.textContent = item.name;
+                    const sourceItem: Element | null = sourceClone.querySelector('.source__item');
+                    if (sourceItem !== null) sourceItem.setAttribute('data-source-id', item.id);
+
+                    fragment.append(sourceClone);
+                }
+            });
+
+            document.querySelector('.sources')?.append(fragment);
+        }
+    }
+}
+
+export default Sources;
