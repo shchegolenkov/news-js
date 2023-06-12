de328c397cf24f1b04049464cb2c8cfd74ebf82f
shchegolenkov <denis.amorales@ya.ru>
2023-06-12 20:38:27 +0300

    feat: add proxy

--- news-JS/src/components/controller/appLoader.ts
+++ news-JS/src/components/controller/appLoader.ts
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: 'd8224f8ad97740c79204eab039943e98', // получите свой ключ https://newsapi.org/
+        super('https://rss-news-api.onrender.com/', {
+            apiKey: 'fcf880bf4e2d4a5fabb387bcc64296af', // получите свой ключ https://newsapi.org/
         });
     }
 }
