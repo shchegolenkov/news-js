ca1f460dcc4f323559f26b0f5274961812f8de17
shchegolenkov <denis.amorales@ya.ru>
2023-06-12 15:02:40 +0300

    feat: migrate loader to ts

--- news-JS/src/components/controller/loader.ts
+++ news-JS/src/components/controller/loader.ts
@@ -1,19 +1,21 @@
 class Loader {
-    constructor(baseLink, options) {
+    private baseLink: string;
+    private options: Record<string, string>;
+    constructor(baseLink: string, options: Record<string, string>) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
-    getResp(
-        { endpoint, options = {} },
-        callback = () => {
+    public getResp(
+        { endpoint, options = {} }: { endpoint: string; options?: Record<string, string> },
+        callback = (): void => {
             console.error('No callback for GET response');
         }
-    ) {
+    ): void {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    private errorHandler(res: Response): Response {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,7 +25,7 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
+    private makeUrl(options: Record<string, string>, endpoint: string): string {
         const urlOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
@@ -34,12 +36,12 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    private load<T>(method: string, endpoint: string, callback: (data: T) => void, options = {}): void {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
-            .then((res) => res.json())
-            .then((data) => callback(data))
-            .catch((err) => console.error(err));
+            .then((res: Response) => res.json())
+            .then((data: T) => callback(data))
+            .catch((err: Error) => console.error(err));
     }
 }
 
